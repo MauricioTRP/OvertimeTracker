63c20fe45df3dff3fda7e251c7bc5163b7d574e1
mfuent <mauriciofb@gmail.com>
2025-04-24 14:42:04 -0400

    skeleton for auth module

---
+++ auth/domain/src/main/java/com/kotlinpl/domain/Domain.kt
@@ -0,0 +1,4 @@
+package com.kotlinpl.domain
+
+class Domain {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,4 @@ rootProject.name = "OTT_multimodule"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":app")
+include(":auth:domain")
